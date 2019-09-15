d30332d4b5737a0b3adf91be059b06587e057b4b
King Elisha <ek.chibueze@gmail.com>
2019-09-15 19:17:50 +0100

    removed config

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI: "mongodb+srv://king_elisha:8!@Qx5FNuV8&+KA@cluster0-iepx9.mongodb.net/test?retryWrites=true&w=majority",
-  mongoURILocal: "mongodb://localhost:27017/mern_intro"
-};
