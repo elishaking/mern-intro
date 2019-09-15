d9c88a34c628891b727de47f891474d9593c6cd4
King Elisha <ek.chibueze@gmail.com>
2019-09-15 18:59:10 +0100

    mongodb connection setup

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const server = express();
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+
+//DB Config
+const db = require('./config/keys').mongoURILocal;
+// Connect to MongoDB through Mongoose
+mongoose.connect(db, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+}).then(() => {
+  console.log("MongoDB connected");
+}).catch(err => console.log(err));
 
 server.use(express.json());
 
