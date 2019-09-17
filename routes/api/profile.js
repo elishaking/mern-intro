6f78f2d04a35ee6f3cd5ed271fe1494ec32129f3
King Elisha <ek.chibueze@gmail.com>
2019-09-17 15:04:18 +0100

    delete profile and user route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -254,4 +254,19 @@ router.delete('/education/:edu_id', passport.authenticate('jwt', { session: fals
     }).catch((err) => res.status(404).json(err));
 });
 
+// @route DELETE api/profile
+// @description Delete user and profile
+// @access Private
+router.delete('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  // Delete the profile
+  Profile.findByIdAndRemove({ user: req.user.id })
+    .then(() => {
+      // Delete the user
+      User.findOneAndRemove({ user: req.user.id })
+        .then(() => {
+          res.json({ success: true });
+        });
+    }).catch((err) => res.status(404).json(err));
+});
+
 module.exports = router;
