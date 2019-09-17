11f3f6d0584fe69580a91ed568b2081788fa203b
King Elisha <ek.chibueze@gmail.com>
2019-09-17 09:58:24 +0100

    create/update profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 
 // Load profile model
-const Profle = require('../../models/Profile');
+const Profile = require('../../models/Profile');
 // Load user model
 const User = require('../../models/User');
 
@@ -23,7 +23,7 @@ router.get('/test', (req, res) => {
 router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
   const errors = {};
 
-  Profle.findOne({ user: req.user.id })
+  Profile.findOne({ user: req.user.id })
     .then((profile) => {
       if (!profile) {
         errors.noprofile = 'There is no profile for this user';
@@ -32,6 +32,60 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
       res.json(profile);
     })
     .catch((err) => res.status(404).json(err));
+});
+
+// @route POST api/profile/
+// @description Create or Edit user profile
+// @access Private
+router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const errors = {};
+
+  // get fields from req.body
+  const profileFields = {};
+  profileFields.user = req.user.id;
+
+  if (req.body.handle) profileFields.handle = req.body.handle;
+  if (req.body.company) profileFields.company = req.body.company;
+  if (req.body.website) profileFields.website = req.body.website;
+  if (req.body.location) profileFields.location = req.body.location;
+  if (req.body.bio) profileFields.bio = req.body.bio;
+  if (req.body.status) profileFields.status = req.body.status;
+  if (req.body.githubusername) profileFields.githubusername = req.body.githubusername;
+  // skills - split comma separated values into array
+  if (typeof req.body.skills !== 'undefined') profileFields.skills = req.body.skills.split(',');
+  // Social
+  profileFields.social = {};
+  if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+  if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+  if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+  if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+  if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+
+  Profile.findOne({ user: req.user.id })
+    .then((profile) => {
+      if (profile) {
+        // Update existing profile
+        Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        ).then((profile) => res.json(profile));
+      } else {
+        // Create profile
+        // Check if handle exists
+        Profile.findOne({ handle: profileFields.handle })
+          .then((profile) => {
+            if (profile) {
+              errors.handle = 'That handle already exists';
+              res.status(400).json(errors);
+            }
+
+            new Profile(profileFields).save().then((profile) => {
+              res.json(profile);
+            });
+          });
+      }
+    });
 });
 
 module.exports = router;
