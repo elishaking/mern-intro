5eb5e19568a1cbef58e342ad5ac6649088d46468
King Elisha <ek.chibueze@gmail.com>
2019-09-17 11:03:32 +0100

    profile field validations

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+// Load Input Validation
+const validateProfileInput = require('../../validation/profile');
+
 // Load profile model
 const Profile = require('../../models/Profile');
 // Load user model
@@ -24,6 +27,7 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
   const errors = {};
 
   Profile.findOne({ user: req.user.id })
+    .populate('user', ['name', 'avatar'])
     .then((profile) => {
       if (!profile) {
         errors.noprofile = 'There is no profile for this user';
@@ -38,7 +42,12 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
 // @description Create or Edit user profile
 // @access Private
 router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
-  const errors = {};
+  const { errors, isValid } = validateProfileInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    res.status(400).json(errors);
+  }
 
   // get fields from req.body
   const profileFields = {};
