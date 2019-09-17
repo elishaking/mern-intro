5d59979850fdafdd545d361409390a90c29bc0c2
King Elisha <ek.chibueze@gmail.com>
2019-09-17 09:07:19 +0100

    current user profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Load profile model
+const Profle = require('../../models/Profile');
+// Load user model
+const User = require('../../models/User');
 
 // @route GET api/profile/test
 // @description Tests profile route
@@ -8,6 +15,23 @@ router.get('/test', (req, res) => {
   res.json({
     msg: "Profile works"
   });
+});
+
+// @route GET api/profile/test
+// @description Get current users profile
+// @access Private
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const errors = {};
+
+  Profle.findOne({ user: req.user.id })
+    .then((profile) => {
+      if (!profile) {
+        errors.noprofile = 'There is no profile for this user';
+        return res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch((err) => res.status(404).json(err));
 });
 
 module.exports = router;
